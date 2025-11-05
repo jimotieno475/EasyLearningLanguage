e3d43574809896a1d2b6430abd1b02f87ceb25da
jimotieno475 <jimotieno475@gmail.com>
2025-11-05 15:17:28 +0300

    change signup to pure polish

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,17 +4,17 @@ export default function Signup() {
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
-      "https://app.activenow.io/external/signup_form/load_by_js?city_id=&code=RAZqWzuQ4MaYNwUi&proficiency_id=&school_id=16752&signup_form_id=160652&venue_id=&zz=";
+      "https://app.activenow.io/external/signup_form/load_by_js?city_id=&code=RAZqWzuQ4MaYNwUi&proficiency_id=&school_id=16752&signup_form_id=165928&venue_id=&zz=";
     script.async = true;
 
-    const container = document.querySelector(".activenow-form-160652-container");
+    const container = document.querySelector(".activenow-form-165928-container");
     if (container) container.appendChild(script);
   }, []);
 
   return (
     <div className="min-h-screen  justify-center items-start py-12">
       {/* IMPORTANT → do NOT modify these class names */}
-      <div className="activenow-form-container activenow-form-160652-container">
+      <div className="activenow-form-container activenow-form-165928-container">
         <center>
           <img
             src="https://www.activenow.io/assets/ripple.gif"
